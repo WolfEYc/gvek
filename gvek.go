f183b33b9602231cc3aa8e904856362ca69bb67a
iwolf <iwolf@eogresources.com>
2025-06-03 16:49:14 -0500

    expect dylib from normal linking spots, maybe i should just bundle all? kinda weird tho

--- gvek.go
+++ gvek.go
@@ -8,14 +8,40 @@ import (
 	"github.com/ebitengine/purego"
 )
 
-func get_dylib_path() string {
+func get_dylib_path(target_microarch string) string {
 	switch runtime.GOOS {
 	case "darwin":
-		return "./libzvek.dylib"
+		switch runtime.GOARCH {
+		// only arm64 macos supported (you can build your own intel macos zvek but why?)
+		case "arm":
+			switch target_microarch {
+			default:
+				return "libzvek-arm64-m1.dylib"
+			}
+		default:
+			panic("arm64 is only supported macos cpu arch")
+		}
 	case "linux":
-		return "./libzvek.so"
+		switch runtime.GOARCH {
+		case "arm":
+			switch target_microarch {
+			default: // maybe should include neoverse-n1 or perhaps even cortex or something for compat
+				return "libzvek-arm64-neoverse-v2.so"
+			}
+		case "amd64":
+			switch target_microarch {
+			case "zen4": // speeeeeeeed!
+				return "libzvek-amd64-zen4.so"
+			default: // still AVX2 so should be decent!
+				return "libzvek-amd64-haswell.so"
+			}
+		default:
+			panic("amd64 and arm are only supported linux cpu arch")
+		}
 	default:
-		// haha screw windows users, not that anything is going to run fast on windows anyways.
+		if runtime.GOOS == "windows" {
+			panic("Windows user DEEEEEETECTED, everyone quick! laugh at them!")
+		}
 		panic(fmt.Errorf("GOOS=%s is not supported", runtime.GOOS))
 	}
 }
@@ -32,8 +58,8 @@ type Apply_Args struct {
 
 var Add_f32 func(args *Apply_Args)
 
-func Init() {
-	lib, err := purego.Dlopen(get_dylib_path(), purego.RTLD_LAZY)
+func Init(target_microarch string) {
+	lib, err := purego.Dlopen(get_dylib_path(target_microarch), purego.RTLD_LAZY)
 	if err != nil {
 		panic(err)
 	}
