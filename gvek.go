516f702d31e0650a326d0c59c99e015c68d11196
WolfEYc <isaacwolfsite@gmail.com>
2025-06-03 23:11:54 -0500

    attempt at making this not have to list every single op!

--- gvek.go
+++ gvek.go
@@ -8,42 +8,9 @@ import (
 	"github.com/ebitengine/purego"
 )
 
-func get_dylib_path(target_microarch string) string {
-	switch runtime.GOOS {
-	case "darwin":
-		switch runtime.GOARCH {
-		// only arm64 macos supported (you can build your own intel macos zvek but why?)
-		case "arm":
-			switch target_microarch {
-			default:
-				return "libzvek-arm64-m1.dylib"
-			}
-		default:
-			panic("arm64 is only supported macos cpu arch")
-		}
-	case "linux":
-		switch runtime.GOARCH {
-		case "arm":
-			switch target_microarch {
-			default: // maybe should include neoverse-n1 or perhaps even cortex or something for compat
-				return "libzvek-arm64-neoverse-v2.so"
-			}
-		case "amd64":
-			switch target_microarch {
-			case "zen4": // speeeeeeeed!
-				return "libzvek-amd64-zen4.so"
-			default: // still AVX2 so should be decent!
-				return "libzvek-amd64-haswell.so"
-			}
-		default:
-			panic("amd64 and arm are only supported linux cpu arch")
-		}
-	default:
-		if runtime.GOOS == "windows" {
-			panic("Windows user DEEEEEETECTED, everyone quick! laugh at them!")
-		}
-		panic(fmt.Errorf("GOOS=%s is not supported", runtime.GOOS))
-	}
+var dylib_ext_map = map[string]string{
+	"darwin": "dylib",
+	"linux":  "so",
 }
 
 type Stream struct {
@@ -56,10 +23,49 @@ type Apply_Args struct {
 	a, b, c Stream
 }
 
+type Op uint
+
+const (
+	Add Op = iota
+	Sub
+	Div
+	Mul
+	Mod
+	Min
+	Max
+	Log
+	Pow
+	// LShift
+	// RShift
+	// SLShift
+	And
+	Or
+	Xor
+)
+
 var Add_f32 func(args *Apply_Args)
 
-func Init(target_microarch string) {
-	lib, err := purego.Dlopen(get_dylib_path(target_microarch), purego.RTLD_LAZY)
+type Number interface {
+	uint8 | int8 | uint16 | int16 | uint32 | int32 | uint64 | int64 | float32 | float64
+}
+type Floats interface {
+	float32 | float64
+}
+type Signed_Numbers interface {
+	int8 | int16 | int32 | int64 | float32 | float64
+}
+
+func Apply[T Number, OP Op](args *Apply_Args) {
+
+}
+
+func Init() {
+	dylib_ext, ok := dylib_ext_map[runtime.GOOS]
+	if !ok {
+		panic(fmt.Errorf("os: %s is not supported", runtime.GOOS))
+	}
+	dylib_name := "libzvek." + dylib_ext
+	lib, err := purego.Dlopen(dylib_name, purego.RTLD_LAZY)
 	if err != nil {
 		panic(err)
 	}
